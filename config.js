b79e3e1293ecabde0f55910dce3508afcfb0da4d
mn15104 <mn15104@my.bristol.ac.uk>
2018-02-12 16:57:56 +0000

    Added config

---
+++ config.js
@@ -0,0 +1,17 @@
+var config = function(){
+
+}
+
+config.username = function(){
+    return "root";
+}
+
+config.password = function(){
+    return "root";
+}
+
+config.db = function(){
+    return "OffMyChest";
+}
+
+module.exports = config;
