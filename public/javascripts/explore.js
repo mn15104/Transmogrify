1af6a85b81dcd73a0d13206d72eaf9a516e82540
mn15104 <mn15104@my.bristol.ac.uk>
2018-03-17 14:05:24 +0000

    updated explore

--- public/javascripts/explore.js
+++ public/javascripts/explore.js
@@ -4,8 +4,19 @@ function init(){
             nextRow();
         }
     });
-    $('.brick').click(function(){
+    $('.flipper').click(function(){
         $(this).toggleClass('flipped');
+    })
+    $('.brick-wall .brick_profile_img').click(function(){
+       var brick = $(this).parent('.brick');
+       var flip = $(this).siblings('.flipper');
+       var eTop = $(this).offset().left; //get the offset top of the element
+       var posx = (eTop - $(document).width()); //position of the ele w.r.t window
+       var xmove = ($(document).width())/2 - posx;
+       $('.brick').not(brick).fadeOut('slow');
+       $(flip).animate({'opacity':'0'}, 'slow');
+       $(this).animate({'opacity':'1',}, 'slow');
+       
     })
 }
 
