ef5e08572b1bad1ca4d44e4535999d871478cc10
mn15104 <mn15104@my.bristol.ac.uk>
2018-04-10 03:27:05 +0100

    Added page sliding transitions to intro page

--- public/javascripts/intro.js
+++ public/javascripts/intro.js
@@ -6,10 +6,14 @@ function init(){
     $('#right-arrow').click(function(){
         $('.title.left').addClass('slide_down');
         $('.title.right').addClass('selected');
+        $('.left_half').animate({left:'50%'}, {duration:2000});
+        $('.right_half').animate({left:'50%'}, {duration:2000});
     });
     $('#left-arrow').click(function(){
-        console.log("hi");
+        
         $('.title.right').addClass('slide_down');
         $('.title.left').addClass('selected');
+        $('.left_half').animate({left:'-50%'}, {duration:2000});
+        $('.right_half').animate({left:'-50%'}, {duration:2000});
     });
 }
