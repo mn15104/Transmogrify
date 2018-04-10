4240de2fca5d506bd100fb01267f2391cb824e01
mn15104 <mn15104@my.bristol.ac.uk>
2018-04-10 18:07:50 +0100

    intro page transitions work great

--- public/javascripts/intro.js
+++ public/javascripts/intro.js
@@ -4,16 +4,22 @@
 function init(){
     console.log("hello");
     $('#right-arrow').click(function(){
+        $('#left-arrow').fadeOut();
         $('.title.left').addClass('slide_down');
         $('.title.right').addClass('selected');
-        $('.left_half').animate({left:'50%'}, {duration:2000});
-        $('.right_half').animate({left:'50%'}, {duration:2000});
+        $('.left_half').delay(2000).animate({left:'0%'}, {duration:2000});
+        $('.right_half').delay(2000).animate({left:'100%'}, {duration:2000, complete: function(){
+            location.href="http://localhost:3000/";
+        }});
     });
     $('#left-arrow').click(function(){
-        
+        $('#right-arrow').fadeOut();
         $('.title.right').addClass('slide_down');
         $('.title.left').addClass('selected');
-        $('.left_half').animate({left:'-50%'}, {duration:2000});
-        $('.right_half').animate({left:'-50%'}, {duration:2000});
+        $('.left_half').delay(2000).animate({left:'-100%'}, {duration:2000});
+        $('.right_half').delay(2000).animate({left:'-50%'}, {duration:2000, complete:function(){
+            location.href="http://localhost:3000/login";
+        }});
+
     });
 }
